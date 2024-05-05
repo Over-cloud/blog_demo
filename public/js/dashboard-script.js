747df730aae944f8a5274dc8f11e86626ee52154
Ruilinzhang0415 <472075013ruilinzhang@gmail.com>
2024-05-05 11:06:08 -0500

    Add feature: keep active section id in localStorage; on page refresh, set active link

--- public/js/dashboard-script.js
+++ public/js/dashboard-script.js
@@ -27,14 +27,26 @@ document.addEventListener('DOMContentLoaded', function(){
         const section = document.getElementById(sectionId);
         if (section) {
             section.classList.add('active');
+            // Store the ID of the active tab-link in localStorage
+            localStorage.setItem('activeSectionId', sectionId);
         }
     }
 
-
+    // Add click event listener to each tab-link
     tabLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
             setActiveLink(this);
         });
     });
+
+    // Retrieve the ID of the active tab-link from localStorage
+    const sectionId = localStorage.getItem('activeSectionId');
+    if (sectionId) {
+        // Find the tab-link with the retrieved ID and trigger a click event
+        const link = document.querySelector(`.tab-link[href="#${sectionId}"]`);
+        if (link) {
+            link.click();
+        }
+    }
 });
