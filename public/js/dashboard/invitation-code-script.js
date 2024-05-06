99513e219c07779f6954fa373de29dbd44b87aca
Ruilinzhang0415 <472075013ruilinzhang@gmail.com>
2024-05-06 11:12:40 -0500

    Add feature: add an overlay for adding invitation codes.

--- public/js/dashboard/invitation-code-script.js
+++ public/js/dashboard/invitation-code-script.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const overlay = document.getElementById('overlay');
+    const addCodeBtn = document.getElementById('add-code-btn');
+    const closeBtn = document.getElementById('close-btn');
     const generateCodeBtn = document.getElementById('generate-invitation-code-btn');
+    const codeDisplay = document.getElementById('generated-code');
+
+    function generateCode() {
+        return code = Math.floor(1000 + Math.random() * 9000);
+    }
+
+    function openOverlay() {
+        overlay.style.display = 'block';
+        codeDisplay.textContent = generateCode();
+    }
+
     generateCodeBtn.addEventListener('click', function(event) {
         event.preventDefault();
-        const code = Math.floor(1000 + Math.random() * 9000);
-        document.getElementById('new-code').value = code;
+        openOverlay();
     });
+
+    function closeOverlay() {
+        overlay.style.display = 'none';
+    }
+
+    closeBtn.addEventListener('click', closeOverlay);
 });
