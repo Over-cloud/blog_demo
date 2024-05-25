1dd1c34d6b201500d44170f22edcb04099429e0b
Ruilinzhang0415 <472075013ruilinzhang@gmail.com>
2024-05-24 20:00:40 -0500

    Add feature: upon opening add-code form, set default start date to today, set default end date to today + 7 days.

--- public/js/dashboard/invitation-code-script.js
+++ public/js/dashboard/invitation-code-script.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function(){
     if (sessionStorage.getItem('codeOverlay') === 'show') {
         openOverlay();
         fillInCode();
+        setDatesToDefault();
 
         const messageHistory = sessionStorage.getItem('message-history');
         if (messageHistory) {
@@ -53,6 +54,7 @@ document.addEventListener('DOMContentLoaded', function(){
         event.preventDefault();
         openOverlay();
         fillInCode();
+        setDatesToDefault();
         hideMessage();
     });
 
@@ -260,5 +262,20 @@ document.addEventListener('DOMContentLoaded', function(){
             style: '',
             content: '',
         }));
+    }
+
+    function parseDateLocal(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    function setDatesToDefault() {
+        let today = new Date();
+        codeValidFrom.value = parseDateLocal(today);
+
+        today.setDate(today.getDate() + 7);
+        codeValidUntil.value = parseDateLocal(today);
     }
 });
